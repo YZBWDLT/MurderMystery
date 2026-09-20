900f5e430dced3eb231e8f261023ca17960da59c
YZBWDLT <yzbwdlt@163.com>
2026-09-20 21:37:05 +0800

    [更改] 进行三包合并（第二三包）

--- behavior_packs/BP_murder_mystery_blocks/scripts/main.js
+++ behavior_packs/BP_murder_mystery_blocks/scripts/main.js
@@ -1,13 +1,10 @@
 // @ts-check
 
 import * as minecraft from "@minecraft/server";
+import * as ui from "@minecraft/server-ui";
+import * as info from "./info";
 
-/**
- * @typedef PaintingPlacerComponent
- * @property {string} id 画使用的 ID。
- * @property {number} [width] 画的宽度。
- * @property {number} [height] 画的高度。
- */
+// #region 常用方法
 
 /** 获取和方块交互后，实际放置的方块位置。
  * @description 专门适用于 interactWithBlock 的前事件和后事件。
@@ -26,6 +23,81 @@ function getPlaceLocation(block, blockFace) {
     };
     return placeLocation[blockFace];
 }
+
+/** 返回实体是否为玩家。
+ * @param {minecraft.Entity} entity
+ * @returns {entity is minecraft.Player}
+ */
+function isPlayer(entity) {
+    if (!entity.isValid) return false;
+    return entity.typeId === "minecraft:player";
+}
+
+/** 将一个形式为`"x y z"`的字符串输出为`Vector3`格式。
+ * @param {string} location
+ * @returns {minecraft.Vector3}
+ */
+function parseString(location) {
+    const parts = location.trim().split(" ");
+    if (parts.length !== 3) throw new Error(`Invalid location format: expected "X Y Z", got "${location}"`);
+
+    const [xStr, yStr, zStr] = parts;
+    const x = Number(xStr);
+    const y = Number(yStr);
+    const z = Number(zStr);
+    if (isNaN(x) || isNaN(y) || isNaN(z)) throw new Error(`Invalid numeric value in location string: "${location}"`);
+
+    return { x, y, z };
+}
+
+/**
+ * @typedef BlockData 表示一个方块的信息。
+ * @property {string} id 方块 ID。
+ * @property {minecraft.Vector3} location 方块位置。
+ * @property {Record<string, boolean | number | string | undefined>} [states] 方块状态。
+ */
+
+/** 在某个位置放置方块。
+ * @param {BlockData} blockData
+ * @param {minecraft.Dimension} dimension
+ * @throws 当试图在未加载区块放置方块时会报错。
+ * @throws 在指定方块状态时，请确保该方块存在这个方块状态！
+ */
+function setBlock(blockData, dimension) {
+    const { id, location, states } = blockData;
+    dimension.setBlockType(location, id);
+    // 设定方块的方块状态
+    const placedBlock = dimension.getBlock(location);
+    if (states && placedBlock) setState(placedBlock, states);
+    return dimension.getBlock(location);
+}
+
+/** 将方块设定为特定的方块状态。
+ * @param {minecraft.Block} block
+ * @param {Record<string, boolean | number | string | undefined>} states
+ */
+function setState(block, states) {
+    Object.entries(states).forEach(([state, value]) => {
+        // @ts-ignore 因为原版的补全文件发疯，没有考虑附加包自定义的状态，所以这里必须忽略报错
+        block.setPermutation(block.permutation.withState(state, value));
+    });
+}
+
+/** @type {minecraft.CustomCommandResult} */
+const executedByNotPlayer = {
+    status: minecraft.CustomCommandStatus.Failure,
+    message: "不能由非玩家执行此命令",
+};
+
+// #endregion
+// #region 自定义画
+
+/**
+ * @typedef PaintingPlacerComponent
+ * @property {string} id 画使用的 ID。
+ * @property {number} [width] 画的宽度。
+ * @property {number} [height] 画的高度。
+ */
 
 minecraft.system.beforeEvents.startup.subscribe(event => {
     event.itemComponentRegistry.registerCustomComponent("painting:placer", {
@@ -77,3 +149,175 @@ minecraft.system.beforeEvents.startup.subscribe(event => {
         },
     });
 });
+
+// #endregion
+// #region 冻结方块
+
+minecraft.system.beforeEvents.startup.subscribe(event => {
+    // 命令声明
+    event.customCommandRegistry.registerCommand(
+        {
+            description: "放置一张地图的冻结方块。",
+            name: "murder_mystery:placefreezeblock",
+            permissionLevel: minecraft.CommandPermissionLevel.GameDirectors,
+            optionalParameters: [{ name: "显示调试性信息", type: minecraft.CustomCommandParamType.Boolean }],
+        },
+        (origin, showDebugInfo = true) => {
+            const player = origin.sourceEntity;
+            if (!player) return executedByNotPlayer;
+            if (!isPlayer(player)) return executedByNotPlayer;
+            minecraft.system.run(() => showSetFreezeBlockUI(player, showDebugInfo));
+        },
+    );
+});
+
+/**
+ * @param {minecraft.Player} player
+ * @param {boolean} showDebugInfo
+ */
+function showSetFreezeBlockUI(player, showDebugInfo) {
+    const form = new ui.CustomForm(player, "放置冻结方块").label(`显示调试性信息：§a${showDebugInfo}`).spacer();
+    Object.keys(info.freezeBlockData).map(mapName => {
+        form.button({ translate: `map.${mapName}` }, () => setFreezeBlock(mapName, showDebugInfo, player)).spacer();
+    });
+    form.show();
+}
+
+/**
+ * @param {string} mapName
+ * @param {boolean} showDebugInfo
+ * @param {minecraft.Player} showPlayer
+ */
+function setFreezeBlock(mapName, showDebugInfo, showPlayer) {
+    const freezeBlockDatas = info.freezeBlockData[mapName];
+    if (!freezeBlockDatas) {
+        showPlayer.sendMessage("§c无法找到此地图。");
+        return;
+    }
+    /** @param {string} message */
+    function sendMessage(message) {
+        if (showDebugInfo) showPlayer.sendMessage(message);
+    }
+    freezeBlockDatas.forEach((freezeBlockData, index) => {
+        // ===== 变量准备 =====
+        const overworld = minecraft.world.getDimension("overworld");
+        const tickingAreaName = `freezeBlock${index}`;
+        let { id, from, to } = freezeBlockData;
+        // 坐标变换
+        const offsetData = info.freezeBlockMapOffset[mapName];
+        if (offsetData) {
+            const { x: originX, y: originY, z: originZ } = offsetData.origin;
+            const { x: realX, y: realY, z: realZ } = offsetData.real;
+            from = { x: from.x + realX - originX, y: from.y + realY - originY, z: from.z + realZ - originZ };
+            to = { x: to.x + realX - originX, y: to.y + realY - originY, z: to.z + realZ - originZ };
+        }
+        // 添加常加载区域
+        minecraft.world.tickingAreaManager.createTickingArea(tickingAreaName, { from, to, dimension: overworld }).then(() => {
+            // 放置冻结方块
+            const fromString = `${from.x} ${from.y} ${from.z}`;
+            const toString = `${to.x} ${to.y} ${to.z}`;
+            sendMessage(`正在尝试放置位于§a${fromString} - ${toString}§r的冻结方块：§a${id}`);
+            try {
+                overworld.fillBlocks(new minecraft.BlockVolume(from, to), id);
+            } catch (error) {
+                if (error instanceof Error)
+                    sendMessage(
+                        `§c在放置位于${fromString} - ${toString}的冻结方块${id}时遇到了错误，请将下面的错误原因汇报给开发者：\n${error.message}`,
+                    );
+            }
+            // 移除常加载区域
+            minecraft.world.tickingAreaManager.removeTickingArea(tickingAreaName);
+        });
+    });
+}
+
+// #endregion
+// #region 自定义头颅
+
+minecraft.system.beforeEvents.startup.subscribe(event => {
+    // 命令声明
+    event.customCommandRegistry.registerCommand(
+        {
+            description: "放置一张地图的头颅。",
+            name: "murder_mystery:placehead",
+            permissionLevel: minecraft.CommandPermissionLevel.GameDirectors,
+            optionalParameters: [{ name: "显示调试性信息", type: minecraft.CustomCommandParamType.Boolean }],
+        },
+        (origin, showDebugInfo = true) => {
+            const player = origin.sourceEntity;
+            if (!player) return executedByNotPlayer;
+            if (!isPlayer(player)) return executedByNotPlayer;
+            minecraft.system.run(() => showSetHeadUI(player, showDebugInfo));
+        },
+    );
+});
+
+/**
+ * @param {minecraft.Player} player
+ * @param {boolean} showDebugInfo
+ */
+function showSetHeadUI(player, showDebugInfo) {
+    const form = new ui.CustomForm(player, "放置头颅").label(`显示调试性信息：§a${showDebugInfo}`);
+    Object.keys(info.headData).map(mapName => {
+        form.spacer().button({ translate: `map.${mapName}` }, () => {
+            setHead(mapName, showDebugInfo, player);
+            form.close();
+        });
+    });
+    form.show();
+}
+
+/**
+ * @param {string} mapName
+ * @param {boolean} showDebugInfo
+ * @param {minecraft.Player} showPlayer
+ */
+function setHead(mapName, showDebugInfo, showPlayer) {
+    const mapHeadDatas = info.headData[mapName];
+    if (!mapHeadDatas) {
+        showPlayer.sendMessage("§c无法找到此地图。");
+        return;
+    }
+    /** @param {string} message */
+    function sendMessage(message) {
+        if (showDebugInfo) showPlayer.sendMessage(message);
+    }
+    mapHeadDatas.forEach((mapHeadData, index) => {
+        // ===== 变量准备 =====
+        const overworld = minecraft.world.getDimension("overworld");
+        const tickingAreaName = `head${index}`;
+        let { id: idWithoutNamespace, location: locationString } = mapHeadData;
+        const id = `player_head:${idWithoutNamespace}`;
+        let location = parseString(locationString);
+        // 坐标变换
+        const offsetData = info.headMapOffset[mapName];
+        if (offsetData) {
+            const { x: originX, y: originY, z: originZ } = parseString(offsetData.origin);
+            const { x: realX, y: realY, z: realZ } = parseString(offsetData.real);
+            location = { x: location.x + realX - originX, y: location.y + realY - originY, z: location.z + realZ - originZ };
+            locationString = `${location.x} ${location.y} ${location.z}`;
+        }
+        // 添加常加载区域
+        minecraft.world.tickingAreaManager
+            .createTickingArea(tickingAreaName, { from: location, to: location, dimension: overworld })
+            .then(() => {
+                // 放置冻结方块
+                sendMessage(`正在尝试放置位于§a${locationString}§r的头颅：§a${id}`);
+                try {
+                    /** @type {Record<string, string | number | boolean | undefined>} */
+                    const states =
+                        "rotation" in mapHeadData
+                            ? { "minecraft:block_face": "up", "minecraft:sixteen_way_rotation": mapHeadData.rotation }
+                            : { "minecraft:block_face": mapHeadData.facing };
+                    setBlock({ id, location, states }, overworld);
+                } catch (error) {
+                    if (error instanceof Error)
+                        sendMessage(
+                            `§c在放置位于${locationString}的头颅${id}时遇到了错误，请将下面的错误原因汇报给开发者：\n${error.message}`,
+                        );
+                }
+                // 移除常加载区域
+                minecraft.world.tickingAreaManager.removeTickingArea(tickingAreaName);
+            });
+    });
+}
